8835c94c433d44d526d7202ad2971d8934daeef7
Pradyut Panda <ppanda@zynga.com>
2013-10-13 22:00:41 -0700

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by ppanda on 10/13/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "xyzAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([xyzAppDelegate class]));
+    }
+}
---
+++ ToDoList/xyzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xyzAppDelegate.h
+//  ToDoList
+//
+//  Created by ppanda on 10/13/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xyzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
